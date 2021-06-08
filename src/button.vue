2814882c6c05f92cb6a9b786429e3fa2a48c1898
你的英文名 <>
2021-06-08 16:50:30 +0800

    更新  button  里面   icon的样式

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-      <svg v-if="icon" class="icon">
-        <use :xlink:href=`#i-${icon}`></use>
-      </svg>
+      <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+
       <div class="content">
         <slot></slot>
       </div>
@@ -15,7 +14,22 @@
 <script>
 export default {
   name: "button.vue",
-  props: ['icon','iconPosition']
+  // props: ['icon','iconPosition']
+  props:{
+    icon:{},
+    iconPosition:{
+      type: String,
+      default:'left',
+      validator(value){
+        console.log(value)
+        if(value !== 'left' && value !== 'right'){
+          return false
+        }else{
+          return true
+        }
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg class="g-icon">
+      <use :xlink:href=`#i-${name}`></use>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "icon",
+  props: ['name']
+}
+</script>
+
+<style scoped lang="scss">
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
