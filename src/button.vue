5974057ebb90ec4e0e2fc73595c8eb2bcfa6569d
你的英文名 <>
2021-06-08 15:38:35 +0800

    加入parcel 加button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button class="g-button">按钮</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "button.vue"
+}
+</script>
+
+<style scoped lang="scss">
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button--height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+
+</style>
