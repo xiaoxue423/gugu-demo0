54627036947829ee5adff92816b29dd94b1d372a
你的英文名 <>
2021-06-10 16:16:41 +0800

    row 和 col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col" :class="[` col- ${span} `]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuguCol',
+  props: {
+    span: [Number,String]
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background: yellow;
+  width: 50%;
+  border: 1px solid red;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    //.col.col-1
+    //.col.col-2
+    &.# {
+      $class-prefix
+    }
+    .# {
+      $n
+    }
+  {
+    width: ($n / 24) * 100%;
+  }
+  }
+}
+</style>
