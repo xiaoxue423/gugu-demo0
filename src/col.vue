9b769b2fdb78f9bb083983af98f500c4006fff79
你的英文名 <>
2021-06-11 11:14:14 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[` col- ${span} `]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, offset && `offset - ${offset}`]"
+       :style="{paddingLeft:gutter/2+'px',paddingRight: gutter/2+'px'}">
+    <div style="border: 1px solid green;height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -8,8 +11,35 @@
 export default {
   name: 'GuguCol',
   props: {
-    span: [Number,String]
-  }
+    span: {
+      type: [Number, String]
+    },
+    // 偏移
+    offset: {
+      type: [Number, String]
+    },
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  data(){
+    return {
+      gutter: 0
+    }
+  },
+  created() {
+    console.log('col created')
+    console.log(this.$children)
+    //没有儿子
+  },
+  mounted() {
+    console.log('col mounted')
+    console.log(this.$children)
+    //有儿子
+   this.$children.forEach((vm)=>{
+     vm.gutter = this.gutter
+   })
+  },
 }
 </script>
 
@@ -19,6 +49,7 @@ export default {
   background: yellow;
   width: 50%;
   border: 1px solid red;
+  padding: 0 10px;
 
   $class-prefix: col-;
   @for $n from 1 through 24 {
@@ -27,12 +58,27 @@ export default {
     &.# {
       $class-prefix
     }
-    .# {
-      $n
-    }
-  {
+  # {
+    $n
+  } {
     width: ($n / 24) * 100%;
   }
   }
+
+  //生成24个offset类
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    //.col.col-1
+    //.col.col-2
+    &.# {
+      $class-prefix
+    }
+  # {
+    $n
+  } {
+    margin-left: ($n / 24) * 100%;
+  }
+  }
+
 }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px' }">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'GuguRow',
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  created() {
+    console.log('row created')
+    console.log(this.$children)
+    //没有儿子
+  },
+  mounted() {
+    console.log('row mounted')
+    console.log(this.$children)
+    //有儿子
+  },
+
+}
+</script>
 
 <style scoped lang="scss">
 .row {
