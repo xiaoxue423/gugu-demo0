8191cd41968ca54bbb2b2d2bc0b954cb3fd486f1
你的英文名 <>
2021-07-06 16:18:28 +0800

    添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,17 +1,40 @@
 <template>
-    <div class="sider">
+<!--  name就是动画的名字-->
+  <transition name="slide">
+    <div class="sider" v-if="visible">
       <slot></slot>
+      <button @click="visible = false">close</button>
     </div>
+  </transition>
 </template>
 
 <script>
-    export default {
-        name: "GuguSider"
+export default {
+  name: "GuguSider",
+  data(){
+    return {
+      visible:true
     }
+  },
+  methods:{
+  }
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .sider {
+  position: relative;
 
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active, .silde-leave-active {
+  transition: all .5s;
+}
+.slide-enter, .slide-leave-to{
+  margin-left: -200px;
 }
 </style>
